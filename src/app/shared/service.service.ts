ab887471662ef6ab16379d79fb2377d3014f8817
GROZiiT <srpeddada@gmail.com>
2022-10-24 16:44:56 -0400

    Internship_project_2

--- src/app/shared/service.service.ts
+++ src/app/shared/service.service.ts
@@ -10,10 +10,10 @@ export class ServiceService {
   selcetedData = new Data;
   constructor(public http: HttpClient) { }
 
-  readonly URL1 = "http://localhost:443/users/register"
-  readonly URL2 = "http://localhost:443/users/login"
-  readonly URL3 = "http://localhost:443/users/update"
-    readonly URL4 = "http://localhost:443/users/getinfo"
+  readonly URL1 = "https://techavant.herokuapp.com/users/register"
+  readonly URL2 = "https://techavant.herokuapp.com/users/login"
+  readonly URL3 = "https://techavant.herokuapp.com/users/update"
+    readonly URL4 = "https://techavant.herokuapp.com/users/getinfo"
 
   postRegister(data: Data) {
     return this.http.post(this.URL1, data, {
